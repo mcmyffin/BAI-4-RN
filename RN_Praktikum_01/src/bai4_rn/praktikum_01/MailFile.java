14af10ae51c384586d572886a03ccc5968b092cd
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-22 17:36:24 +0200

    Added properties.

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/MailFile.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/MailFile.java
@@ -3,7 +3,8 @@ package bai4_rn.praktikum_01;
 import bai4_rn.praktikum_01.client.ClientData;
 import bai4_rn.praktikum_01.client.ClientDataFactory;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.Properties;
 
 import static bai4_rn.praktikum_01.util.FileUtils.decodeFile;
 import static bai4_rn.praktikum_01.util.FileUtils.encodeFile;
@@ -12,6 +13,23 @@ import static bai4_rn.praktikum_01.util.FileUtils.encodeFile;
  * Created by Andreas on 22.10.2015.
  */
 public class MailFile {
+    public static ClientData createClientData(String configurationFile) throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = new FileInputStream(configurationFile);
+        properties.loadFromXML(inputStream);
+
+        String mailAddress = properties.getProperty("mailAddress");
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+        String hostname = properties.getProperty("hostname");
+        int port = Integer.valueOf(properties.getProperty("port"));
+
+        inputStream.close();
+
+        ClientData clientData = ClientDataFactory.create(mailAddress, username, password, hostname, port);
+        return clientData;
+    }
+
     public static void test() {
         try {
 //            String encodedFile = encodeFile("...");
@@ -19,17 +37,10 @@ public class MailFile {
 //            String fileData = new String(bytes);
 //            System.out.println(fileData);
 
-            String mailAddress = "rnp2015@informatik.haw-hamburg.de";
-            String username = "rnp2015";
-            String password = "Aufgabe1";
-            String hostname = "mailgate.informatik.haw-hamburg.de";
-            int port = 25;
-
-            ClientData clientData = ClientDataFactory.create(mailAddress, username, password, hostname, port);
+            ClientData clientData = createClientData("config.xml");
             clientData.writeToServer("Test");
             String s = clientData.readFromServer();
             System.out.println(s);
-
         } catch (IOException e) {
             e.printStackTrace();
         }
