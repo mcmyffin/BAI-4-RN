cbd0c53e2f35e89b4d353a2838baf418c661080c
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-22 15:39:22 +0200

    Implemented FileUtils.java

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/util/FileUtils.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/util/FileUtils.java
@@ -1,6 +1,14 @@
 package bai4_rn.praktikum_01.util;
 
+import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
+
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static com.google.common.base.Preconditions.*;
 
 /**
  * Created by Andreas on 22.10.2015.
@@ -9,11 +17,15 @@ public class FileUtils {
     private FileUtils() {
     }
 
-    public static String encodeFile(String filePath) throws FileNotFoundException {
-        throw new UnsupportedOperationException();
+    public static String encodeFile(String filePath) throws IOException {
+        checkNotNull(filePath);
+
+        Path path = Paths.get(filePath);
+        byte[] binaryData = Files.readAllBytes(path);
+        return Base64.encode(binaryData);
     }
 
     public static byte[] decodeFile(String encodedFile) {
-        throw new UnsupportedOperationException();
+        return Base64.decode(encodedFile);
     }
 }
