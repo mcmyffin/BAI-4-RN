7b86360be4d744f26c48ce89ed89c9c434dd0d17
mcmuffin <dimitri.meier@haw-hamburg.de>
2015-10-22 18:28:45 +0200

    finish CommandUtils

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/command/CommandUtils.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/command/CommandUtils.java
@@ -4,25 +4,38 @@ import bai4_rn.praktikum_01.client.ClientData;
 import bai4_rn.praktikum_01.mail.Mail;
 
 import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
 
 /**
  * Created by Andreas on 22.10.2015.
  */
 public class CommandUtils {
+
+    private static Mail mail;
+    private static ClientData clientData;
+
     public static Mail getMail() {
-        throw new UnsupportedOperationException();
+
+        checkNotNull(mail);
+        return mail;
     }
 
-    public static void setMail(Mail mail) {
-        throw new UnsupportedOperationException();
+    public static void setMail(Mail aMail) {
+
+        checkNotNull(aMail);
+        mail = aMail;
     }
 
     public static ClientData getClientData() {
-        throw new UnsupportedOperationException();
+
+        checkNotNull(clientData);
+        return clientData;
     }
 
-    public static void setClientData(ClientData clientData) {
-        throw new UnsupportedOperationException();
+    public static void setClientData(ClientData aClientData) {
+
+        checkNotNull(aClientData);
+        clientData = aClientData;
     }
     
     public static ClientResponse createClientResponse(String type) {
@@ -32,6 +45,15 @@ public class CommandUtils {
 
     public static ServerReply createServerReply(String inputLine) {
         checkNotNull(inputLine);
-        throw new UnsupportedOperationException();
+        String statusCodeStr = inputLine.substring(0,3);
+
+        int statusCodeInt =  Integer.parseInt(statusCodeStr);
+        for (ServerReplayEnum elem : ServerReplayEnum.values()) {
+
+            if(elem.getStatusCode() == statusCodeInt) return elem;
+        }
+
+        checkState(false);
+        return null;
     }
 }
