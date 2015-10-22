9ba67b162b78437a6d8f1598db0f1adb81f6d2ab
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-22 15:46:41 +0200

    Added JavaDoc and preconditions in FileUtils.java

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/util/FileUtils.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/util/FileUtils.java
@@ -2,13 +2,12 @@ package bai4_rn.praktikum_01.util;
 
 import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static com.google.common.base.Preconditions.*;
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Created by Andreas on 22.10.2015.
@@ -17,6 +16,12 @@ public class FileUtils {
     private FileUtils() {
     }
 
+    /**
+     * Encodes an file with Base64
+     * @param filePath file to encode
+     * @return encoded file data as String object
+     * @throws IOException if file was found or is not readable
+     */
     public static String encodeFile(String filePath) throws IOException {
         checkNotNull(filePath);
 
@@ -25,7 +30,13 @@ public class FileUtils {
         return Base64.encode(binaryData);
     }
 
+    /**
+     * Decoded file data with Base64
+     * @param encodedFile encoded file data
+     * @return decoded file data as byte array
+     */
     public static byte[] decodeFile(String encodedFile) {
+        checkNotNull(encodedFile);
         return Base64.decode(encodedFile);
     }
 }
