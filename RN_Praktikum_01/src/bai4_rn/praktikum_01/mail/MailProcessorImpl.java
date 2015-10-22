2442df1b812f18a7df31fd7cd3d9b55df44ca641
mcmuffin <dimitri.meier@haw-hamburg.de>
2015-10-22 15:35:39 +0200

    MailProcessor constructor created

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/mail/MailProcessorImpl.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/mail/MailProcessorImpl.java
@@ -1,13 +1,22 @@
 package bai4_rn.praktikum_01.mail;
 
+import bai4_rn.praktikum_01.client.ClientData;
+
 /**
  * Created by abp615 on 22.10.2015.
  */
 public class MailProcessorImpl implements MailProcessor {
 
+    private ClientData clientData;
+
+    public MailProcessorImpl(ClientData data) {
+
+        this.clientData = clientData;
+    }
 
     @Override
     public void run() {
 
+        // TODO
     }
 }
