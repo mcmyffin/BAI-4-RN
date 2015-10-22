a58e06c12124b71aa019abd742c514afc4fb3821
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-22 17:10:57 +0200

    Restored client data and mail data accessors in CommandUtils.java

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/command/CommandUtils.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/command/CommandUtils.java
@@ -9,6 +9,22 @@ import static com.google.common.base.Preconditions.checkNotNull;
  * Created by Andreas on 22.10.2015.
  */
 public class CommandUtils {
+    public static Mail getMail() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setMail(Mail mail) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static ClientData getClientData() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setClientData(ClientData clientData) {
+        throw new UnsupportedOperationException();
+    }
+    
     public static ClientResponse createClientResponse(String type) {
         checkNotNull(type);
         return ClientResponseEnum.valueOf(type);
