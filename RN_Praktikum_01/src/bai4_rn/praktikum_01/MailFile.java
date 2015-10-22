b4c001f91caec925428609f7f4cb217dd894a41d
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-22 18:31:10 +0200

    Implemented main, but not finished.

--- RN_Praktikum_01/src/bai4_rn/praktikum_01/MailFile.java
+++ RN_Praktikum_01/src/bai4_rn/praktikum_01/MailFile.java
@@ -2,12 +2,17 @@ package bai4_rn.praktikum_01;
 
 import bai4_rn.praktikum_01.client.ClientData;
 import bai4_rn.praktikum_01.client.ClientDataFactory;
+import bai4_rn.praktikum_01.mail.Mail;
+import bai4_rn.praktikum_01.mail.MailImpl;
+import bai4_rn.praktikum_01.mail.MailProcessorImpl;
+import bai4_rn.praktikum_01.util.FileUtils;
 
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
-import static bai4_rn.praktikum_01.util.FileUtils.decodeFile;
-import static bai4_rn.praktikum_01.util.FileUtils.encodeFile;
+import static com.google.common.base.Preconditions.checkArgument;
 
 /**
  * Created by Andreas on 22.10.2015.
@@ -30,23 +35,32 @@ public class MailFile {
         return clientData;
     }
 
-    public static void test() {
-        try {
-//            String encodedFile = encodeFile("...");
-//            byte[] bytes = decodeFile(encodedFile);
-//            String fileData = new String(bytes);
-//            System.out.println(fileData);
+    public static void main(String[] args) {
+        String recipientMailAddress = null;
+        String attachmentFilePath = null;
+        String subject = "Im sending that file!";
 
+        if (args.length != 2) {
+            recipientMailAddress = args[0];
+            attachmentFilePath = args[1];
+        } else {
+            checkArgument(false);
+        }
+
+        try {
             ClientData clientData = createClientData("config.xml");
-            clientData.writeToServer("Test");
-            String s = clientData.readFromServer();
-            System.out.println(s);
+            String encodedAttachment = FileUtils.encodeFile(attachmentFilePath);
+            String contentType = FileUtils.getContentType(attachmentFilePath);
+            Mail mail = new MailImpl(
+                    clientData.getMailAddress(),
+                    recipientMailAddress,
+                    subject,
+                    encodedAttachment,
+                    contentType);
+
+            MailProcessorImpl mailProcessor = new MailProcessorImpl(clientData);
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    public static void main(String[] args) {
-        test();
     }
 }
