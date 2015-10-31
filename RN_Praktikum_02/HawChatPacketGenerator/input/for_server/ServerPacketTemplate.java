a76e5e06c9fa0cf200eb73952de579c25622cb6c
MrTorgue <andreas.berks@haw-hamburg.de>
2015-10-31 16:13:55 +0100

    Added empty packet generator with server template files.

---
+++ RN_Praktikum_02/HawChatPacketGenerator/input/for_server/ServerPacketTemplate.java
@@ -0,0 +1,19 @@
+package de.haw_chat.server.network.packets;
+
+import static de.haw_chat.common.operation.implementations.OperationDataManager.getOperationData;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class #{OPERATION_NAME}Packet extends AbstractServerPacket {
+    #{ATTRIBUTES}
+
+    public #{OPERATION_NAME}Packet(#{CONSTRUCTOR_PARAMETERS}) {
+        #{CONSTRUCTOR_INIT_ATTRIBUTES}
+    }
+
+    @Override
+    public String toMessageString() {
+        return #{MESSAGE_STRING};
+    }
+}
---
+++ RN_Praktikum_02/HawChatPacketGenerator/src/de/haw_chat_generator/Main.java
@@ -0,0 +1,10 @@
+package de.haw_chat_generator;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
