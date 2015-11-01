7cfb98045fa2b76156929d3fffdaeef2dde7566d
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-01 23:14:21 +0100

    Added new templates for generator. Temporally added source folder of generator to gitignore, because the source is to ugly...

---
+++ RN_Praktikum_02/HawChatPacketGenerator/input/for_client/ClientPacketTemplate.java
@@ -0,0 +1,19 @@
+package de.haw_chat.client.network.packets.client_packets;
+
+import static de.haw_chat.common.operation.implementations.OperationDataManager.getOperationData;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class #{OPERATION_NAME}Packet extends AbstractClientPacket {
+    #{ATTRIBUTES}
+
+    public #{OPERATION_NAME}Packet(#{CONSTRUCTOR_PARAMETERS}) {
+        #{CONSTRUCTOR_INIT_ATTRIBUTES}
+    }
+
+    @Override
+    public String toMessageString() {
+        return #{MESSAGE_STRING};
+    }
+}
---
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/packets/client_packets/RegisterAccountPacket.java
@@ -0,0 +1,28 @@
+package de.haw_chat.server.network.packets.client_packets;
+
+import de.haw_chat.server.network.interfaces.ChatClientThread;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class RegisterAccountPacket extends AbstractClientPacket {
+    private String username;
+    private String password;
+
+    public RegisterAccountPacket(ChatClientThread chatClientThread, String messageString) {
+        super(chatClientThread);
+        this.username = messageString.split(" ")[1];
+        this.password = messageString.split(" ")[2];
+    }
+
+    @Override
+    public void process() {
+        // TODO: Implement processing logic
+        
+        // NOTES:
+        // - you can access client data with: getClientData()
+        // - you can access global server data with: getServerData()
+        // - you can send response to client with: sendToClient(ServerPacket)
+        throw new UnsupportedOperationException();
+    }
+}
