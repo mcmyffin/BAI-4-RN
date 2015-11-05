07bbe753d9c90c2c29c82f396e13588d9fa3d2be
MrTorgue <andreas.berks@haw-hamburg.de>
2015-11-05 21:20:32 +0100

    Changed type of status codes from int to Status.

---
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/packets/client_packets/UnsupportedClientPacket.java
@@ -0,0 +1,29 @@
+package de.haw_chat.server.network.packets.client_packets;
+
+import de.haw_chat.server.network.interfaces.ChatClientThread;
+import de.haw_chat.common.operation.implementations.Status;
+
+import static de.haw_chat.common.operation.implementations.Status.*;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class UnsupportedClientPacket extends AbstractClientPacket {
+    
+
+    public UnsupportedClientPacket(ChatClientThread chatClientThread, String messageString) {
+        super(chatClientThread);
+        
+    }
+
+    @Override
+    public void process() {
+        // TODO: Implement processing logic
+        
+        // NOTES:
+        // - you can access client data with: getClientData()
+        // - you can access global server data with: getServerData()
+        // - you can send response to client with: sendToClient(ServerPacket)
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/packets/server_packets/UnknownServerPacket.java
@@ -0,0 +1,22 @@
+package de.haw_chat.server.network.packets.server_packets;
+
+import de.haw_chat.common.operation.implementations.Status;
+
+import static de.haw_chat.common.operation.implementations.Status.*;
+import static de.haw_chat.common.operation.implementations.OperationDataManager.getOperationData;
+
+/**
+ * Created by Andreas on 31.10.2015.
+ */
+public class UnknownServerPacket extends AbstractServerPacket {
+    
+
+    public UnknownServerPacket() {
+        
+    }
+
+    @Override
+    public String toMessageString() {
+        return "" + getOperationCode();
+    }
+}
