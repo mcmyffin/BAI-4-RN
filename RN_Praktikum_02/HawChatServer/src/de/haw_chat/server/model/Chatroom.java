c956d2752f7561451c17760cfbf2ea310bd1e2f1
mcmyffin <dimitri.meier@haw-haburg.de>
2015-11-10 15:35:49 +0100

    add Chatroom Model

---
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/model/Chatroom.java
@@ -0,0 +1,17 @@
+package de.haw_chat.server.model;
+
+/**
+ * Created by dima on 04.11.15.
+ */
+public class Chatroom {
+
+    private int id;
+    private String owner;
+
+    private String chatName;
+    private String password;
+
+    private int maxUser;
+
+
+}
