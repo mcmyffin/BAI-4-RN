8f182351658f121e8564e2cc1f3a0b7e3a58c5a8
mcmyffin <dimitri.meier@haw-haburg.de>
2015-11-12 14:38:25 +0100

    add chatroom information to clientData

--- RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/implementations/ClientDataImpl.java
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/implementations/ClientDataImpl.java
@@ -2,13 +2,22 @@ package de.haw_chat.server.network.implementations;
 
 import de.haw_chat.server.network.interfaces.ClientData;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
 /**
  * Created by Andreas on 31.10.2015.
  */
 final class ClientDataImpl implements ClientData {
+
     private String username;
+    private Set<String> connectedChatNames;
 
     private ClientDataImpl() {
+        this.connectedChatNames = new HashSet();
     }
 
     public static ClientData create() {
@@ -23,5 +32,22 @@ final class ClientDataImpl implements ClientData {
     @Override
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Set<String> getConnectedChats() {
+        return Collections.unmodifiableSet(this.connectedChatNames);
+    }
+
+    @Override
+    public void addConnectedChats(String chatname) {
+        checkNotNull(chatname);
+        this.connectedChatNames.add(chatname);
+    }
+
+    @Override
+    public void disconnectFromChat(String chatname) {
+        checkNotNull(chatname);
+        this.connectedChatNames.remove(chatname);
     }
 }
--- RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/interfaces/ClientData.java
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/network/interfaces/ClientData.java
@@ -1,9 +1,16 @@
 package de.haw_chat.server.network.interfaces;
 
+import java.util.Set;
+
 /**
  * Created by Andreas on 31.10.2015.
  */
 public interface ClientData {
+
     String getUsername();
     void setUsername(String username);
+
+    Set<String> getConnectedChats();
+    void addConnectedChats(String chatname);
+    void disconnectFromChat(String chatname);
 }
