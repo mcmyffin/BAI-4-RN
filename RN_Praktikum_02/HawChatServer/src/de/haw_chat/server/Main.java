4334fba7a82e0f7278c04ecfb02b18afb4531313
mcmyffin <dimitri.meier@haw-haburg.de>
2015-11-18 17:55:33 +0100

    add buggy main :)

--- RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/Main.java
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/Main.java
@@ -8,9 +8,31 @@ import de.haw_chat.server.network.interfaces.ServerConfiguration;
  * Created by Andreas on 31.10.2015.
  */
 public class Main {
+
+    private static int defaultPort       = 12345;
+    private static int defaultMaxClients = 10;
+    private static boolean defaultSsl    = false;
+
+    private static String c_help = "-h";
+    private static String usage  =  "-- USAGE --\n"+
+                                    "   *.jar [OPTION] <PORT> <MAX_CLIENTS> <SSL>\n\n"+
+                                    "   [OPTION]        : "+c_help+"\n" +
+                                    "   <PORT>          : port number. Default 12345\n"+
+                                    "   <MAX_CLIENTS>   : maximum Client connections\n"+
+                                    "   <SSL>           : 1 for ssl enable or 0 for disable. Default 0\n";
+
+    private static void showUsage(){
+        System.out.println(usage);
+        System.exit(1);
+    }
+
+//    private
+
     public static void main(String[] args) {
+
+        if(args.length != 0 && args.length < 3) showUsage();
         ServerConfiguration configuration =
-                DeviceFactory.createChatServerConfiguration(12345, 10, false);
+                DeviceFactory.createChatServerConfiguration(defaultPort, defaultMaxClients, defaultSsl);
         Server server = DeviceFactory.createChatServer(configuration);
 
         Thread thread = new Thread(server);
