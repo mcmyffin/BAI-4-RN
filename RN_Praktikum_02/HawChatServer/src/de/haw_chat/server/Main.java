646be73ce5dcab25050b11d54f8dd2dd23beff06
mcmyffin <dimitri.meier@haw-haburg.de>
2015-11-18 18:29:46 +0100

    added usage for arguments and exit command while server running

--- RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/Main.java
+++ RN_Praktikum_02/HawChatServer/src/de/haw_chat/server/Main.java
@@ -4,6 +4,8 @@ import de.haw_chat.server.network.implementations.DeviceFactory;
 import de.haw_chat.server.network.interfaces.Server;
 import de.haw_chat.server.network.interfaces.ServerConfiguration;
 
+import java.util.Scanner;
+
 /**
  * Created by Andreas on 31.10.2015.
  */
@@ -13,13 +15,21 @@ public class Main {
     private static int defaultMaxClients = 10;
     private static boolean defaultSsl    = false;
 
-    private static String c_help = "-h";
+    private static final String opt_help =  "-h";
+    private static final String com_exit =  "exit";
+
     private static String usage  =  "-- USAGE --\n"+
                                     "   *.jar [OPTION] <PORT> <MAX_CLIENTS> <SSL>\n\n"+
-                                    "   [OPTION]        : "+c_help+"\n" +
+                                    "   [OPTION]        : "+ opt_help +"\n" +
                                     "   <PORT>          : port number. Default 12345\n"+
                                     "   <MAX_CLIENTS>   : maximum Client connections\n"+
                                     "   <SSL>           : 1 for ssl enable or 0 for disable. Default 0\n";
+
+    private static void showUsage(String message){
+        System.out.println(message);
+        System.out.println(usage);
+        System.exit(1);
+    }
 
     private static void showUsage(){
         System.out.println(usage);
@@ -31,6 +41,18 @@ public class Main {
     public static void main(String[] args) {
 
         if(args.length != 0 && args.length < 3) showUsage();
+        else if(args.length == 1 && args[0] == opt_help) showUsage();
+        else if(args.length == 3){
+            try{
+                defaultPort = Integer.parseInt(args[0]);
+                defaultPort = Integer.parseInt(args[1]);
+                defaultSsl  = Integer.parseInt(args[2]) == 1;
+            }catch (NumberFormatException ex){
+                showUsage(">> INVALID ARGUMENT FORMAT");
+            }
+        }
+
+        Scanner sc = new Scanner(System.in);
         ServerConfiguration configuration =
                 DeviceFactory.createChatServerConfiguration(defaultPort, defaultMaxClients, defaultSsl);
         Server server = DeviceFactory.createChatServer(configuration);
@@ -39,5 +61,11 @@ public class Main {
         thread.start();
 
         System.out.println("SERVER STARTED!");
+
+        String readedLine = null;
+        while(true){
+            readedLine = sc.nextLine();
+            if(readedLine.equals(com_exit)) System.exit(0);
+        }
     }
 }
